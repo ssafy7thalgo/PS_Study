556c19c185468aa62e2be19c7a4f7171488862bb
Geun Ji <jg6735@naver.com>
2022-03-15 00:24:16 +0900

    BOJ 2667번 Silver1 단지 번호 붙이기

--- BJ/0314_BJ_2667/BJ_2667_단지번호붙이기_지근.java
+++ BJ/0314_BJ_2667/BJ_2667_단지번호붙이기_지근.java
@@ -13,8 +13,6 @@ public class BOJ_2667_단지번호붙이기 {
     static int[][] map; // 지도 배열
     static boolean[][] visited; // 방문 체크용 배열
     static int[][] deltas = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}}; // 상, 하, 좌, 우
-    static int[] moveY = {-1, 0, 1, 0};
-    static int[] moveX = {0, 1, 0, -1};
     static int count = 1; // 단지 번호 카운트
 
     public static void main(String[] args) throws IOException {
@@ -73,8 +71,8 @@ public class BOJ_2667_단지번호붙이기 {
 
         // 4방 탐색
         for (int d = 0; d < 4; d++) {
-            int nx = x + moveX[d];
-            int ny = y + moveY[d];
+            int nx = x + deltas[d][0];
+            int ny = y + deltas[d][1];
 
             // 배열 범위를 벗어나면 넘어가기
             if (nx >= N || ny >= N || nx < 0 || ny < 0) {
@@ -85,58 +83,6 @@ public class BOJ_2667_단지번호붙이기 {
             if (map[nx][ny] == 1 && !visited[nx][ny]) {
                 dfs(nx, ny);
             }
-        }
-    }
-
-    static void bfs(int x, int y) {
-        Queue<Point> queue = new LinkedList<Point>();
-        Point p = new Point(x, y);
-        queue.offer(p);
-
-        while (!queue.isEmpty()) {
-            Point cur = queue.poll();
-            int curX = cur.getX();
-            int curY = cur.getY();
-            visited[curY][curX] = true;
-
-            // 4방 탐색
-            for (int d = 0; d < 4; d++) {
-                int nextX = curX + moveX[d];
-                int nextY = curY + moveY[d];
-
-                // 지도 범위를 벗어나면 넘어가기
-                if (nextX >= N || nextY >= N || nextX < 0 || nextY < 0) {
-                    continue;
-                }
-
-                // 다음 지역에 집이 있고, 방문하지 않았다면
-                if (map[nextY][nextX] != 0 && !visited[nextY][nextX]) {
-                    // 해당 좌표로 객체 생성
-                    cur = new Point(nextX, nextY);
-                    // 큐에 삽입
-                    queue.offer(cur);
-                    // 해당 좌표 방문 체크
-                    visited[curY][curX] = true;
-                }
-            }
-        }
-    }
-
-    static class Point {
-        private int x;
-        private int y;
-
-        public Point(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        public int getX() {
-            return x;
-        }
-
-        public int getY() {
-            return y;
         }
     }
 }
